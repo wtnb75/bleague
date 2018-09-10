cb6f8db06e343d880cdbcadbd6c05f2dffdc4ba2
Takashi WATANABE <wtnb75@gmail.com>
2018-09-10 23:19:56 +0900

    remove print

--- blg2.py
+++ blg2.py
@@ -82,8 +82,8 @@ class bleague2ical2:
             for games in rlist.find_class("game_list"):
                 for game in games.find_class("gamedata_left"):
                     home, away = [x.text_content().strip() for x in game.find_class("team")]
-                    print("home", home, "away", away)
-                    print("text", self.text1(game, "point"))
+                    # print("home", home, "away", away)
+                    # print("text", self.text1(game, "point"))
                     try:
                         homept, awaypt = self.text1(game, "point").split("-")
                     except ValueError:
